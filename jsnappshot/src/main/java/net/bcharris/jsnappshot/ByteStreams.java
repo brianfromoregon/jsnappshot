f8a701489845de638add05ebdd60b0383b07d266
medotin <medotin@8dd6c968-065a-11df-8158-c7cbc0dd6240>
2010-01-23 04:04:00 +0000

    pull out ByteStreams to outer level

---
+++ jsnappshot/src/main/java/net/bcharris/jsnappshot/ByteStreams.java
@@ -0,0 +1,49 @@
+package net.bcharris.jsnappshot;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// excerpts from guava-libraries which isn't released yet
+class ByteStreams {
+
+    private static final int BUF_SIZE = 0x1000; // 4K
+
+    /**
+     * Copies all bytes from the input stream to the output stream.
+     * Does not close or flush either stream.
+     *
+     * @param from the input stream to read from
+     * @param to the output stream to write to
+     * @return the number of bytes copied
+     * @throws IOException if an I/O error occurs
+     */
+    public static long copy(InputStream from, OutputStream to) throws IOException {
+        byte[] buf = new byte[BUF_SIZE];
+        long total = 0;
+        while (true) {
+            int r = from.read(buf);
+            if (r == -1) {
+                break;
+            }
+            to.write(buf, 0, r);
+            total += r;
+        }
+        return total;
+    }
+
+    /**
+     * Reads all bytes from an input stream into a byte array.
+     * Does not close the stream.
+     *
+     * @param in the input stream to read from
+     * @return a byte array containing all the bytes from the stream
+     * @throws IOException if an I/O error occurs
+     */
+    public static byte[] toByteArray(InputStream in) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        copy(in, out);
+        return out.toByteArray();
+    }
+}
